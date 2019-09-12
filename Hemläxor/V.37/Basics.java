d9c3d9a8b8b8ae383fafbcfd0a0b3f230dc2a15d
Leoholst <leo.holst@elev.ga.ntig.se>
2019-09-12 10:55:35 +0200

    V.37 nr.3

--- Hemläxor/V.37/Basics.java
+++ Hemläxor/V.37/Basics.java
@@ -4,6 +4,7 @@ public class Basics {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		
+		System.out.println("Vad heter du?");
+		String namn = input.nextLine();
 	}
 }
