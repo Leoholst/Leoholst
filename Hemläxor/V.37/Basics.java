1efe1c78b2b517597d3f2855dd9b9e7ef16b4bf8
Leoholst <leo.holst@elev.ga.ntig.se>
2019-09-19 10:56:59 +0200

    V.38 nr1

--- Hemläxor/V.37/Basics.java
+++ Hemläxor/V.37/Basics.java
@@ -4,17 +4,24 @@ public class Basics {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Vad heter du?");
+		System.out.println("Vad heter du ?");
 		String name = input.nextLine();
-		System.out.println("Hur gammal är du?");
+		System.out.println("Hur gammal är du ?");
 		String age = input.nextLine();
-		System.out.println("Vad är din adress?");
+		System.out.println("Vad är din adress ?");
 		String address = input.nextLine();
-		System.out.println("Vad är ditt postnummer?");
+		System.out.println("Vad är ditt postnummer ?");
 		String postcode = input.nextLine();
-		System.out.println("Vilken stad bor du i?");
+		System.out.println("Vilken stad bor du i ?");
 		String city = input.nextLine();
-		System.out.println("Vad är ditt telefonnummer?");
+		System.out.println("Vad är ditt telefonnummer ?");
 		String phone = input.nextLine();
+		System.out.println("");
+		System.out.println("Information:");
+		System.out.println("Namn:    " + name);
+		System.out.println("Ålder:   " + age + " år");
+		System.out.println("Adress:  " + address);
+		System.out.println("         " + postcode + " " + city);
+		System.out.println("Telefon: " + phone);
 	}
 }
---
+++ Hemläxor/V.38/V38.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class V38 {
+	public static void main(String[] args) {
+
+		Scanner  input = new Scanner(System.in);
+		System.out.println("Skriv 10 heltal:");
+		int nr1 = input.nextInt();
+		int nr2 = input.nextInt();
+		int nr3 = input.nextInt();
+		int nr4 = input.nextInt();
+		int nr5 = input.nextInt();
+		int nr6 = input.nextInt();
+		int nr7 = input.nextInt();
+		int nr8 = input.nextInt();
+		int nr9 = input.nextInt();
+		int nr10 = input.nextInt();
+		
+		System.out.println();
+		System.out.println((nr1 + nr2 + nr3 + nr4 + nr5 + nr6 + nr7 +nr8 + nr9 + nr10) /10);
+		
+		
+		
+	}
+}
