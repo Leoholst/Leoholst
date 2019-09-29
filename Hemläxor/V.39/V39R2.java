ea51e684f6e180d51484b5ea469ccac4c9c34c45
Leoholst <leo.holst@elev.ga.ntig.se>
2019-09-29 16:11:06 +0200

    V39 R2

--- Hemläxor/V.39/V39R2.java
+++ Hemläxor/V.39/V39R2.java
@@ -4,6 +4,16 @@ public class V39R2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		int R1 = 0;
+		int R2 = 0;
+		int S = 0;
 		
+		R1 = input.nextInt();
+		S = input.nextInt();
+		
+		if (-1000 < R1 && R1 < 1000 && -1000 < S && S < 1000) {
+			R2 = S * 2 - R1;
+			System.out.println(R2);
+		}
 	}
 }
