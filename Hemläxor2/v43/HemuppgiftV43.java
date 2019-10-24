cd952e440559999b0b9cc0929f7f5c016c01fec1
Leoholst <leo.holst@elev.ga.ntig.se>
2019-10-24 10:37:32 +0200

    HemuppgiftV43 andra commit

--- Hemläxor2/v43/HemuppgiftV43.java
+++ Hemläxor2/v43/HemuppgiftV43.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 public class HemuppgiftV43 {
 	public static void main(String[] args) {
-		System.out.println(myMethod(2));
+			System.out.println(volume(0));
+			System.out.println(reverse(null));
 	}
-	public static double myMethod(int radius) {
-		double pi = 3.14;
-		return 4*pi*radius*radius*radius/3;
+	public static double volume(double radius) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Skriv ditt favorit nummer!");
+		radius = input.nextInt();
+		return 4*Math.PI*Math.pow(radius, 3)/3;
+	}
+	public static String reverse(String str) {
+		Scanner input = new Scanner(System.in);
+		System.out.println(" ");
+		System.out.println("Skriv ditt favorit ord!");
+		str = input.nextLine();
+		String word = new StringBuffer(str).reverse().toString();
+		return word;
 	}
 }
