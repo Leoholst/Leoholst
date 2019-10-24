cbcac0236f85eb02f0303f36a4029f783e3ed247
Leoholst <leo.holst@elev.ga.ntig.se>
2019-10-24 11:45:29 +0200

    HemuppgiftV43 tredje commit, tredje metoden

--- Hemläxor2/v43/HemuppgiftV43.java
+++ Hemläxor2/v43/HemuppgiftV43.java
@@ -2,21 +2,35 @@ import java.util.Scanner;
 
 public class HemuppgiftV43 {
 	public static void main(String[] args) {
-			System.out.println(volume(0));
-			System.out.println(reverse(null));
+			System.out.println("Volymen på ett klot med ditt favoritnummer som radien är: " + volume(0));
+			System.out.println("Visste du att ditt favoritord baklänges är: " + reverse(null));
+			System.out.println("Du tänker på hur många s det finns i Ishockeymålvaktsskyddsförsäljare! " + "Det är: " + count("Ishockeymålvaktsskyddsförsäljare",'s'));
 	}
 	public static double volume(double radius) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Skriv ditt favorit nummer!");
+		System.out.println("Skriv ditt favoritnummer!");
 		radius = input.nextInt();
 		return 4*Math.PI*Math.pow(radius, 3)/3;
 	}
 	public static String reverse(String str) {
 		Scanner input = new Scanner(System.in);
 		System.out.println(" ");
-		System.out.println("Skriv ditt favorit ord!");
+		System.out.println("Skriv ditt favoritord!");
 		str = input.nextLine();
 		String word = new StringBuffer(str).reverse().toString();
 		return word;
+	}
+	public static int count(String str,char c) {
+		Scanner input = new Scanner(System.in);
+		System.out.println(" ");
+		System.out.println("Jag vet vad du tänker på just nu!");
+		System.out.println(" ");
+		int num = 0;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == c) {
+				num++;
+			}
+		}
+		return num;
 	}
 }
