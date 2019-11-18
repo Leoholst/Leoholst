f7869cf7b41ff0e089d638022cc11ab966c23b14
Leoholst <leo.holst@elev.ga.ntig.se>
2019-11-18 11:30:15 +0100

    Gjorde fler metoder

--- Hemläxor2/v.45/JavaPhysicsLibrary.java
+++ Hemläxor2/v.45/JavaPhysicsLibrary.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class JavaPhysicsLibrary {
 	
-	static double G = 9.82;
+	static double G = 6.67*Math.pow(10, -11);
 	static double R = 8.3144621;
 	static double p_0 = 1013.25;
 	static double c = 299792458;
@@ -10,6 +10,19 @@ public class JavaPhysicsLibrary {
 	
 	public static void main(String[] args) {
 		System.out.println(fahrenheitToCelsius(2));
+		System.out.println(kelvinToCelsius(300));
+		System.out.println(fluidPressure(FluidTable.WATER,10));
+		System.out.println(pressureUnderWater(10));
+		System.out.println(kineticEnergy(2,2));
+		System.out.println(potentialEnergy(2,5));
+		System.out.println(fallSpeed(2.5));
+		System.out.println(delta(1,10));
+		System.out.println(volumeToMass(FluidTable.WATER,1));
+		System.out.println(volumeToMass(GasTable.AIR,1));
+		System.out.println(volumeToMass(SolidTable.IRON,1));
+		System.out.println(svtVelocity(10,5));
+		System.out.println(svtDistance(10,5));
+		System.out.println(svtTime(10,2));
 	}
 	
 	public static double fahrenheitToCelsius(double fahrenheit) {
@@ -18,55 +31,68 @@ public class JavaPhysicsLibrary {
 	}
 	
 	public static double kelvinToCelsius(double kelvin) {
+	return (kelvin - 273.15);
 	//kelvinToCelsius(0) => -273.15
 	}
 	
 	public static double fluidPressure(FluidTable fluid, double deep) {
+	return (fluid.density * g_swe * deep);
 	//fluidPressure(FluidTable.WATER, 10) => 98003,6
 	}
 	
 	public static double pressureUnderWater(double deep) {
-	pressureUnderWater(10) => 98003,6
+	return (FluidTable.WATER.density * g_swe * deep);
+	//pressureUnderWater(10) => 98003,6
 	}
 	
 	public static double kineticEnergy(double mass, double velocity) {
-	kineticEnergy(2,2) => 4
+	return ((mass * Math.pow(velocity, 2)) / 2);
+	//kineticEnergy(2,2) => 4
 	}
 	
 	public static double potentialEnergy(double mass, double height) {
-	potentialEnergy(2,5) => 98.2
+	return (mass * g_swe * height);
+	//potentialEnergy(2,5) => 98.2
 	}
 	
 	public static double fallSpeed(double height) {
+	return (Math.sqrt(2*g_swe*height));
 	//fallSpeed(2.5) => 7,00713922
 	}
 	
 	public static double delta(double first, double last) {
+	return (last - first);
 	//delta(1,10) => 9
 	//delta(5,1) => -4
 	}
 	
 	public static double volumeToMass(FluidTable fluid, double volume) {
+	return (fluid.density * volume);
 	//volumeToMass(FluidTable.WATER,1) => 998
 	}
 	
 	public static double volumeToMass(GasTable gas, double volume) {
+	return (gas.density * volume);
 	//volumeToMass(GasTable.AIR,1) => 1.29
 	}
 	
-	double volumeToMass(SolidTable solid, double volume) {
+	public static double volumeToMass(SolidTable solid, double volume) {
+	return (solid.density * volume);
 	//volumeToMass(SolidTable.IRON,1) => 7870
 	}
 	
 	public static double svtVelocity(double distance, double time) {
+	return (distance / time);
 	//svtVelocity(10,5) => 2
 	}
 	
 	public static double svtDistance(double velocity, double time) {
+	return (velocity * time);
 	//svtDistance(10,5) => 50
 	}
 	
 	public static double svtTime(double distance, double velocity) {
+	return (velocity / distance);
 	//svtTime(10,2) => 5
 	}
 	
