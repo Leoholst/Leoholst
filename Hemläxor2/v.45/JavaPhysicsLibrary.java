f57647dcee601964f197c43136defbe6d78e770b
Leoholst <leo.holst@elev.ga.ntig.se>
2019-11-21 10:44:00 +0100

    Skrev klart 20:e metoden, snyggade till koden och skrev javadoc på några metoder.

--- Hemläxor2/v.45/JavaPhysicsLibrary.java
+++ Hemläxor2/v.45/JavaPhysicsLibrary.java
@@ -2,121 +2,173 @@ import java.util.Scanner;
 
 public class JavaPhysicsLibrary {
 	
-	static double G = 6.67*Math.pow(10, -11);
+	static double G = 6.67 * Math.pow(10, -11);
 	static double R = 8.3144621;
 	static double p_0 = 1013.25;
 	static double c = 299792458;
 	static double g_swe = 9.82;
 	
 	public static void main(String[] args) {
-		System.out.println(fahrenheitToCelsius(2));
-		System.out.println(kelvinToCelsius(300));
-		System.out.println(fluidPressure(FluidTable.WATER,10));
-		System.out.println(pressureUnderWater(10));
-		System.out.println(kineticEnergy(2,2));
-		System.out.println(potentialEnergy(2,5));
-		System.out.println(fallSpeed(2.5));
-		System.out.println(delta(1,10));
-		System.out.println(volumeToMass(FluidTable.WATER,1));
-		System.out.println(volumeToMass(GasTable.AIR,1));
-		System.out.println(volumeToMass(SolidTable.IRON,1));
-		System.out.println(svtVelocity(10,5));
-		System.out.println(svtDistance(10,5));
-		System.out.println(svtTime(10,2));
+		
 	}
 	
+	/** Method 1
+	 * This method converts fahrenheit to celsius.
+	 * @param fahrenheit
+	 * @return
+	 */
 	public static double fahrenheitToCelsius(double fahrenheit) {
-	return (9 * fahrenheit /5 + 32);
-	//fahrenheitToCelsius(50) => 10
+		return (9 * fahrenheit /5 + 32);
 	}
 	
+	/** Method 2
+	 * This method converts kelvin to celsuis.
+	 * @param kelvin
+	 * @return
+	 */
 	public static double kelvinToCelsius(double kelvin) {
-	return (kelvin - 273.15);
-	//kelvinToCelsius(0) => -273.15
+		return (kelvin - 273.15);
 	}
 	
+	/** Method 3
+	 *  This method calculates the fluidpressure on a fluid by a deep.
+	 * @param fluid
+	 * @param deep
+	 * @return
+	 */
 	public static double fluidPressure(FluidTable fluid, double deep) {
-	return (fluid.density * g_swe * deep);
-	//fluidPressure(FluidTable.WATER, 10) => 98003,6
+		return (fluid.density * g_swe * deep);
 	}
 	
+	/** Method 4
+	 *  This method calculates the pressure under water by a specific deep.
+	 * @param deep
+	 * @return
+	 */
 	public static double pressureUnderWater(double deep) {
-	return (FluidTable.WATER.density * g_swe * deep);
-	//pressureUnderWater(10) => 98003,6
+		return (FluidTable.WATER.density * g_swe * deep);
 	}
 	
+	/** Method 5
+	 *  This method uses the formula for kinetic energy to calculate the impetus.  
+	 * @param mass
+	 * @param velocity
+	 * @return
+	 */
 	public static double kineticEnergy(double mass, double velocity) {
-	return ((mass * Math.pow(velocity, 2)) / 2);
-	//kineticEnergy(2,2) => 4
+		return ((mass * Math.pow(velocity, 2)) / 2);
 	}
 	
+	/** Method 6
+	 *  This method uses the formula for potential energy to calculate the impetus.
+	 * @param mass
+	 * @param height
+	 * @return
+	 */
 	public static double potentialEnergy(double mass, double height) {
-	return (mass * g_swe * height);
-	//potentialEnergy(2,5) => 98.2
+		return (mass * g_swe * height);
 	}
 	
+	/** Method 7
+	 *  The method calculates the speed when falling from a specific height.
+	 * @param height
+	 * @return
+	 */
 	public static double fallSpeed(double height) {
-	return (Math.sqrt(2*g_swe*height));
-	//fallSpeed(2.5) => 7,00713922
+		return (Math.sqrt(2*g_swe*height));
 	}
 	
+	/** Method 8
+	 *  The method determines delta.
+	 * @param first
+	 * @param last
+	 * @return
+	 */
 	public static double delta(double first, double last) {
-	return (last - first);
-	//delta(1,10) => 9
-	//delta(5,1) => -4
+		return (last - first);
 	}
 	
+	/** Method 9
+	 *  This method converts a fluids volume to mass.
+	 * @param fluid
+	 * @param volume
+	 * @return
+	 */
 	public static double volumeToMass(FluidTable fluid, double volume) {
-	return (fluid.density * volume);
-	//volumeToMass(FluidTable.WATER,1) => 998
+		return (fluid.density * volume);
 	}
 	
+	/** Method 10
+	 *  The method converts the volume of a gas to mass.
+	 * @param gas
+	 * @param volume
+	 * @return
+	 */
 	public static double volumeToMass(GasTable gas, double volume) {
-	return (gas.density * volume);
-	//volumeToMass(GasTable.AIR,1) => 1.29
+		return (gas.density * volume);
 	}
 	
+	/** Method 11
+	 *  The method converts the volume of a solid substance to mass.
+	 * @param solid
+	 * @param volume
+	 * @return
+	 */
 	public static double volumeToMass(SolidTable solid, double volume) {
-	return (solid.density * volume);
-	//volumeToMass(SolidTable.IRON,1) => 7870
+		return (solid.density * volume);
 	}
+	
 	
 	public static double svtVelocity(double distance, double time) {
-	return (distance / time);
+		return (distance / time);
 	//svtVelocity(10,5) => 2
 	}
 	
+	
 	public static double svtDistance(double velocity, double time) {
-	return (velocity * time);
+		return (velocity * time);
 	//svtDistance(10,5) => 50
 	}
 	
+	
 	public static double svtTime(double distance, double velocity) {
-	return (velocity / distance);
+		return (velocity / distance);
 	//svtTime(10,2) => 5
 	}
 	
+	
 	public static double work(double force, double distance) {
+		return (force * distance);
 	//work(50,10) => 500
 	}
 	
-	double power(double work, double time) {
+	
+	public static double power(double work, double time) {
+		return (work / time);
 	//power(1000,2) => 500
 	}
 	
+	
 	public static double heat(SolidTable solid, double mass, double deltaT) {
+		return (solid.heatCapacity * mass * deltaT);
 	//heat(SolidTable.IRON,1,2) => 900
 	}
 	
+	
 	public static double heat(FluidTable fluid, double mass, double deltaT) {
+		return (fluid.heatCapacity * mass * deltaT);
 	//heat(FluidTable.WATER,1,10) => 41900
 	}
 	
+	
 	public static double heat(GasTable gas, double mass, double deltaT) {
+		return (gas.heatCapacity * mass * deltaT);
 	//heat(GasTable.AIR,1,1) => 1010
 	}
 	
+	
 	public static double velocityToHeight(double velocity) {
+		return (Math.pow(Math.sin(1.57079633), 2) * Math.pow(velocity, 2)/ (2 * g_swe));
 	//velocityToHeight(9.82) => 4.91
 	}
 }
