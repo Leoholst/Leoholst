2bdbcf7d44eed543b41be11167086bf7dd14e76a
Leoholst <leo.holst@elev.ga.ntig.se>
2020-03-18 15:53:51 +0100

    Gjorde välkomstmeddelandet och instruktionerna, funderar på att utöka dem senare.

--- Hemläxor2/v12Talspelet/Talspelet.java
+++ Hemläxor2/v12Talspelet/Talspelet.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class Talspelet {
 
 	public static void main(String[] args) {
-	
+		System.out.println(greetings());
+		System.out.println(System.lineSeparator() + instructions());
 	}
 	
 	public static String greetings() {
-		
+		String welcome = "Welcome to Talspelet!" + System.lineSeparator() + "This game is all about guessing numbers!";
+		return welcome;
 	}
 	
 	public static String instructions() {
-		
+		String instructions = "Instructions" + System.lineSeparator() + "First off you are going to choose a difficulty, there are three different to choose from ranging from easy to hard." + System.lineSeparator() + "After that comes the guessing part! Simply write one number from inside the difficulties range. If you write the wrong one you have to guess again." + System.lineSeparator() + "In order to win you just have to guess the right number, easy right? But watch out! You only have three tries before you run out of guesses and lose!";
+		return instructions;
 	}
-	
+	/*
 	public static int chooseDifficulty() {
 		
 	}
@@ -37,4 +40,12 @@ public class Talspelet {
 	public static String condolences() {
 		
 	}
+	
+	public static int doYouWantToQuit() {
+		
+	}
+	
+	public static int doYouWantToGoAgain() {
+		
+	}*/
 }
