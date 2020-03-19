f8bb400e84e59b60a1613614f6c2d1e35df7f2dc
Leoholst <leo.holst@elev.ga.ntig.se>
2020-03-19 11:05:05 +0100

    Skrev lite mer kod, påbörjade chooseDifficulty metoden.

--- Hemläxor2/v12Talspelet/Talspelet.java
+++ Hemläxor2/v12Talspelet/Talspelet.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Talspelet {
 
 	public static void main(String[] args) {
 		System.out.println(greetings());
 		System.out.println(System.lineSeparator() + instructions());
+		System.out.println(System.lineSeparator() + chooseDifficulty);
 	}
 	
 	public static String greetings() {
@@ -16,11 +18,25 @@ public class Talspelet {
 		String instructions = "Instructions" + System.lineSeparator() + "First off you are going to choose a difficulty, there are three different to choose from ranging from easy to hard." + System.lineSeparator() + "After that comes the guessing part! Simply write one number from inside the difficulties range. If you write the wrong one you have to guess again." + System.lineSeparator() + "In order to win you just have to guess the right number, easy right? But watch out! You only have three tries before you run out of guesses and lose!";
 		return instructions;
 	}
-	/*
-	public static int chooseDifficulty() {
-		
-	}
 	
+	public static Random chooseDifficulty() {
+		Scanner input = new Scanner(System.in);
+		int choose = input.nextInt();
+		int easy = 1;
+		int medium = 2;
+		int hard = 3;
+		if(choose==easy) {
+			Random difficult = (Random) new Random().ints(easy, hard + 1).iterator();
+			return difficult;
+		}
+		if(choose==medium) {
+			
+		}
+		if(choose==hard) {
+			
+		}
+	}
+	/*
 	public static void errorMessage() {
 		
 	}
