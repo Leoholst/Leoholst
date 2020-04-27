af536c2293f537eee079dd1cf46b6d183835e38b
Leoholst <leo.holst@elev.ga.ntig.se>
2020-04-27 10:35:09 +0200

    Jag skapade själva skelettet, dvs. jag skrev ut metoderna jag skrev i planeringen.

---
+++ Hemläxor2/v18Hängagubben/Hängagubben.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Hängagubben {
+	public static Scanner input = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void game() {
+		
+	}
+	
+	public static void againOrQuit() {
+		
+	}
+	
+	public static String welcome() {
+		
+	}
+	
+	public static String instructions() {
+		
+	}
+	
+	public static int chooseDifficulty() {
+		
+	}
+	
+	public static String setEasyDifficulty() {
+		
+	}
+	
+	public static String setHardDifficulty() {
+		
+	}
+	
+	public static char guess() {
+		
+	}
+	
+	public static char rightGuess() {
+		
+	}
+	
+	public static char wrongGuess() {
+		
+	}
+	
+	public static String youWin() {
+		
+	}
+	
+	public static String youLose() {
+		
+	}
+}
