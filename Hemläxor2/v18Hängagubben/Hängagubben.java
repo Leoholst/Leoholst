3a61474f0a2bb4160fdf6f9a9fd9352c84fd9a14
Leoholst <leo.holst@elev.ga.ntig.se>
2020-04-27 11:09:03 +0200

    Skrev innehållet av welcome() och instructions()

--- Hemläxor2/v18Hängagubben/Hängagubben.java
+++ Hemläxor2/v18Hängagubben/Hängagubben.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Hängagubben {
 	public static Scanner input = new Scanner(System.in);
 	
+	
 	public static void main(String[] args) {
-		
+		game();
 	}
 	
 	public static void game() {
-		
+		System.out.println(welcome());
+		System.out.println(System.lineSeparator() + instructions());
 	}
 	
 	public static void againOrQuit() {
@@ -16,13 +18,18 @@ public class Hängagubben {
 	}
 	
 	public static String welcome() {
-		
+		String playerGreeting = "Welcome to Hangman! In this game you will be guessing letters in an unknown word, sounds simple enough? " +
+        System.lineSeparator()  + "Well watch out, with every letter you guess wrong one line will be drawn on the man until he is hanged and you lose!";
+		return playerGreeting;
 	}
 	
 	public static String instructions() {
-		
+		String instructions = "The rules are easy, simply write a letter and you will either see it pop up in the word or see a line being drawn." +
+		System.lineSeparator() + "When the man is hanged you lose! But when you have guessed all the letters in the word you win!" + 
+		System.lineSeparator() + "There is two difficulties to choose from, easy and hard. Easy is shorter words while hard is longer.";
+		return instructions;
 	}
-	
+	/*
 	public static int chooseDifficulty() {
 		
 	}
@@ -54,4 +61,5 @@ public class Hängagubben {
 	public static String youLose() {
 		
 	}
+	*/
 }
